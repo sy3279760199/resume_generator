2184e2c7593bc4eee076ef707f251d757cdb775f
Aeliana <rossiaeliana@gmail.com>
2026-04-07 01:22:57 +0800

    resume editor with preview but not PDF export

--- resume-vue/src/components/sections/SkillSection.vue
+++ resume-vue/src/components/sections/SkillSection.vue
@@ -1,20 +1,88 @@
 <script setup>
 import { ref } from 'vue'
 
+const props = defineProps({
+  skills: Array
+})
+
+const emit = defineEmits(['add-skill', 'remove-skill'])
+
 const newSkill = ref('')
-const emit = defineEmits(['add-skill'])
+const newSkillDesc = ref('')
 
 function addSkill() {
   if (newSkill.value.trim() !== '') {
-    emit('add-skill', newSkill.value)
+    emit('add-skill', 
+    { name: newSkill.value, 
+      desc: newSkillDesc.value })
     newSkill.value = ''
+    newSkillDesc.value = ''
   }
+}
+
+function removeSkill(index) {
+  emit('remove-skill', index)
 }
 </script>
 
 <template>
-    <div class="skill">
-        <input v-model="newSkill" placeholder="请添加技能" />
-        <button @click="addSkill">添加</button>
+  <div class="skill">
+    <h4>技能</h4>
+
+    <!-- 输入 + 添加 -->
+    <div class="input-row">
+      <input v-model="newSkill" placeholder="请添加技能" />
+      <button class="btn btn-primary" @click="addSkill">添加</button>
     </div>
+
+    <!-- 技能描述 -->
+  <div class="input-row">
+    <input v-model="newSkillDesc" placeholder="技能描述（可选）" />
+  </div>
+
+  <!-- 技能列表 -->
+    <li  v-for="(skill, index) in props.skills" :key="index">
+      <div class="skill-item">
+            <div>{{ skill.name }}</div>
+            <div class="desc">{{ skill.desc }}</div>
+        </div>
+      <button class="btn btn-danger" @click="removeSkill(index)">删除</button>
+    </li>
+
+  </div>
 </template>
+
+<style scoped>
+.skill {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  background: rgba(255,255,255,0.6);
+  backdrop-filter: blur(12px);
+  border: 1px solid rgba(255,255,255,0.4);
+
+  border-radius: 16px;
+  padding: 24px;
+
+  box-shadow: 0 10px 30px rgba(0,0,0,0.08);
+}
+
+.skill li {
+  display: flex;
+  align-items: center;
+  gap: 20px; /* 技能和删除按钮间距 */
+}
+
+.skill-item {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+}
+
+.desc {
+  font-size: 12px;
+  color: #888;
+}
+
+</style>
